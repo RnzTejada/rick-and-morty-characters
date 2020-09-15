c1354343c1de76acaee7065d84fb522c7f2567f9
RnzTejada <rnzetejada@gmail.com>
2020-09-14 21:10:32 -0300

    Updates

---
+++ precache-manifest.12ff90b51d580b5ab64f4bb8bfe79b6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b5fe46ba837031c53587e8e2cfdb88",
+    "url": "/rick-and-morty-characters/index.html"
+  },
+  {
+    "revision": "8aacc1a2c4072aee191a",
+    "url": "/rick-and-morty-characters/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "eb8b4767085253ee9a65",
+    "url": "/rick-and-morty-characters/static/css/main.24ce6cb2.chunk.css"
+  },
+  {
+    "revision": "8aacc1a2c4072aee191a",
+    "url": "/rick-and-morty-characters/static/js/2.f8a114ff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rick-and-morty-characters/static/js/2.f8a114ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8b4767085253ee9a65",
+    "url": "/rick-and-morty-characters/static/js/main.e87ac4a1.chunk.js"
+  },
+  {
+    "revision": "3feb22bdf7fb99f96c5e",
+    "url": "/rick-and-morty-characters/static/js/runtime-main.168d6867.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/rick-and-morty-characters/static/media/logo.42d6efdb.png"
+  }
+]);
